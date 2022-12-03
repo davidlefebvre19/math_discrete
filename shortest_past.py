2b35b6fb24d49e97a683eb71d955b40aad2667af
Gloria <gloria.Akli-kodjo@student.uclouvain.be>
2022-12-03 21:35:24 +0100

    j'ai enlever dtype dans la création de distance dans mon algopcq sinon ça fonctionne pas quand tu lui donne des grands chiffres pcq LE PRBL C'EST LA CONVERSTION DE GRAND NOMBRE EN INT  Y A PAS SE PROBLEME POUR LA CONV EN FLOAT
    du coup j'ai aussi modif loadtxt pcq le dtype est pas bon

--- shortest_past.py
+++ shortest_past.py
@@ -11,11 +11,11 @@ def Djisktra(arr):
 
 
 def Bellman_Ford(arr):
-    distance = np.matrix(np.zeros((len(arr), len(arr)),dtype=int))  # initialisation de la matrice D à zero partout
+    distance = np.matrix(np.zeros((len(arr), len(arr))))  # initialisation de la matrice D à zero partout
     for ligne in range(len(arr)):
         for colonne in range(arr[ligne].size):
             if ligne != colonne:  # lorsque le nœud source et le nœud d'arrivé ne sont pas les mêmes (a vers a, b vers b, etc.)
-                distance[ligne, colonne] = 10**5  # mettre la distance entre les deux nœuds = inf
+                distance[ligne, colonne] = 10**12  # mettre la distance entre les deux nœuds = inf
 
     for source in range(len(arr)):  # permettra d'itérer sur la même ligne plusieurs fois
         for _ in range(len(arr) - 1):  # permettra de comparer tous les chemins possibles dans les autres lignes de la matrice d'adjacence
@@ -36,7 +36,7 @@ if __name__ == '__main__':
         print("Please insert the path of the adjacency matrix as an argument")
         sys.exit("Program end")
     print("Adjacency matrix path: " + sys.argv[1])
-    arr = np.loadtxt(sys.argv[1], delimiter=",", dtype=int)
+    arr = np.loadtxt(sys.argv[1], delimiter=",", dtype=float)
     print(arr)
     print("By default, the shortest path of every pair will be calculated using Djisktra, Bellman-Ford and "
           "Floyd_Warshall")
