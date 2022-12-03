be7558e847b6a568866279ae769fbf4380d47cc5
Gloria <gloria.Akli-kodjo@student.uclouvain.be>
2022-12-03 21:23:11 +0100

    ajouter mon algo mais ATTENTION TROUVER UNE SOLUCE POUR LE 10^12 car j'ai aussi modifier 10^12 dans mon algo par 10^5

--- shortest_past.py
+++ shortest_past.py
@@ -11,7 +11,19 @@ def Djisktra(arr):
 
 
 def Bellman_Ford(arr):
-    return
+    distance = np.matrix(np.zeros((len(arr), len(arr)),dtype=int))  # initialisation de la matrice D à zero partout
+    for ligne in range(len(arr)):
+        for colonne in range(arr[ligne].size):
+            if ligne != colonne:  # lorsque le nœud source et le nœud d'arrivé ne sont pas les mêmes (a vers a, b vers b, etc.)
+                distance[ligne, colonne] = 10**5  # mettre la distance entre les deux nœuds = inf
+
+    for source in range(len(arr)):  # permettra d'itérer sur la même ligne plusieurs fois
+        for _ in range(len(arr) - 1):  # permettra de comparer tous les chemins possibles dans les autres lignes de la matrice d'adjacence
+            for node in range(len(arr)):
+                for neighbour in range(arr[node].size):
+                    distance[source, neighbour] = min(distance[source, neighbour], distance[source, node] + arr[node, neighbour])  # si la distance entre le nœud et son voisin est plus faible que celle actuelle, la remplacer
+
+    return distance
 
 
 def Floyd_Warshall(arr):
@@ -25,16 +37,16 @@ if __name__ == '__main__':
         sys.exit("Program end")
     print("Adjacency matrix path: " + sys.argv[1])
     arr = np.loadtxt(sys.argv[1], delimiter=",", dtype=int)
-    # print(arr)
+    print(arr)
     print("By default, the shortest path of every pair will be calculated using Djisktra, Bellman-Ford and "
           "Floyd_Warshall")
     try:
         if n == 3:
             Djisktra(arr)
         else:
-            Djisktra(arr)
-            Bellman_Ford(arr)
-            Floyd_Warshall(arr)
+            print(Djisktra(arr))
+            print(Bellman_Ford(arr))
+            print(Floyd_Warshall(arr))
         print("Shortest path computation done !")
     except:
         print("Shortest path computation error !")
