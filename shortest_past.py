a09ea9a637ccbbd545bbef9d2e4b3b36480e6167
Gloria <gloria.Akli-kodjo@student.uclouvain.be>
2022-12-21 23:55:15 +0100

    minime changement

--- shortest_past.py
+++ shortest_past.py
@@ -10,20 +10,22 @@ def Djisktra(arr):
     path = np.zeros(len(arr), dtype=int)
     path.fill(10**12)
 
+
 def Bellman_Ford(arr):
-    distance = np.matrix(np.zeros((len(arr), len(arr))))  # initialisation de la matrice D à zero partout
+    distance = np.zeros((len(arr), len(arr)))  # initialisation de la matrice D à zero partout
     for ligne in range(len(arr)):
-        for colonne in range(arr[ligne].size):
+        for colonne in range(len(arr)):
             if ligne != colonne:  # lorsque le nœud source et le nœud d'arrivé ne sont pas les mêmes (a vers a, b vers b, etc.)
                 distance[ligne, colonne] = 10**12  # mettre la distance entre les deux nœuds = inf
 
-    for source in range(len(arr)):  # permettra d'itérer sur la même ligne plusieurs fois
+    for source in range(len(arr)):  # permettra d'itérer sur la même ligne plusieurs fois pour pouvoir comparer un même nœud avec tout les autres
         for _ in range(len(arr) - 1):  # permettra de comparer tous les chemins possibles dans les autres lignes de la matrice d'adjacence
-            for node in range(len(arr)):
-                for neighbour in range(arr[node].size):
-                    distance[source, neighbour] = min(distance[source, neighbour], distance[source, node] + arr[node, neighbour])  # si la distance entre le nœud et son voisin est plus faible que celle actuelle, la remplacer
+            for node in range(len(arr)):  # on boucle sur toutes les lignes (nœuds) de la matrice d'adjacence
+                for neighbour in range(len(arr)):
+                    # si la distance entre le nœud et le voisin est plus faible que celle actuelle, la remplacer
+                    distance[source, neighbour] = min(distance[source, neighbour], distance[source, node] + arr[node, neighbour])
 
-    return distance
+    return distance.astype(int)
 
 
 def Floyd_Warshall(arr):
@@ -36,7 +38,7 @@ if __name__ == '__main__':
         print("Please insert the path of the adjacency matrix as an argument")
         sys.exit("Program end")
     print("Adjacency matrix path: " + sys.argv[1])
-    arr = np.loadtxt(sys.argv[1], delimiter=",", dtype=float)
+    arr = np.loadtxt(sys.argv[1], delimiter=",")
     print(arr)
     print("By default, the shortest path of every pair will be calculated using Djisktra, Bellman-Ford and "
           "Floyd_Warshall")
